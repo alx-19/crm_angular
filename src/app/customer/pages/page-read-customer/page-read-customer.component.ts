b01f128d0353f50a0b7281fb5b886a2de110d9e3
Yann COADOUR <yann.coadour@gmail.com>
2022-06-14 00:35:47 +0200

    Page Customer, page read sans css !

--- src/app/customer/pages/page-read-customer/page-read-customer.component.ts
+++ src/app/customer/pages/page-read-customer/page-read-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CustomerService} from "../../services/customer.service";
+import {CustomerM} from "../../../shared/models/customer-m";
 
 
 @Component({
@@ -8,11 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageReadCustomerComponent implements OnInit {
 
+  id:any;
   public titreHeader = "CLIENTS";
+  data!: CustomerM ;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private customerService : CustomerService) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.getItemById();
+  }
+
+  getItemById(){
+  this.customerService.getItemById(this.id).subscribe(data=>{
+    this.data = data;
+    console.log(this.data)
+  })
+  }
 }
