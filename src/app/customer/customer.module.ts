036f0d2084146b517055f6bd9e736f4924079076
Yann COADOUR <yann.coadour@gmail.com>
2022-06-15 16:07:05 +0200

    Design + page en trop suppr + user ok

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
-import { PageCreateCustomerComponent } from './pages/page-create-customer/page-create-customer.component';
 import { PageListCustomersComponent } from './pages/page-list-customers/page-list-customers.component';
 import { PageReadCustomerComponent } from './pages/page-read-customer/page-read-customer.component';
-import { PageUpdateCustomerComponent } from './pages/page-update-customer/page-update-customer.component';
 import {SharedModule} from "../shared/shared.module";
 import {IconsModule} from "../icons/icons.module";
 import {MatButtonModule} from "@angular/material/button";
@@ -13,10 +11,8 @@ import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    PageCreateCustomerComponent,
     PageListCustomersComponent,
     PageReadCustomerComponent,
-    PageUpdateCustomerComponent
   ],
     imports: [
         CommonModule,
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { PageCreateUserComponent } from './pages/page-create-user/page-create-user.component';
 import { PageListUsersComponent } from './pages/page-list-users/page-list-users.component';
 import { PageReadUserComponent } from './pages/page-read-user/page-read-user.component';
-import { PageUpdateUserComponent } from './pages/page-update-user/page-update-user.component';
 import {SharedModule} from "../shared/shared.module";
 import {IconsModule} from "../icons/icons.module";
 import {MatButtonModule} from "@angular/material/button";
@@ -13,10 +11,8 @@ import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    PageCreateUserComponent,
     PageListUsersComponent,
     PageReadUserComponent,
-    PageUpdateUserComponent
   ],
     imports: [
         CommonModule,
