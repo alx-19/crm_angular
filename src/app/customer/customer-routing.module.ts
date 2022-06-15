33a99e1ca8dd806ff6129e6fa59d6dd4899d48fb
Yann COADOUR <yann.coadour@gmail.com>
2022-06-15 16:07:05 +0200

    Design + page en trop suppr + user ok

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PageListCustomersComponent} from "./pages/page-list-customers/page-list-customers.component";
-import {PageCreateCustomerComponent} from "./pages/page-create-customer/page-create-customer.component";
 import {PageReadCustomerComponent} from "./pages/page-read-customer/page-read-customer.component";
-import {PageUpdateCustomerComponent} from "./pages/page-update-customer/page-update-customer.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'list', pathMatch:'full'},
   {path: 'list', component: PageListCustomersComponent},
-  {path: 'create', component: PageCreateCustomerComponent},
   {path: 'list/customers/:id', component: PageReadCustomerComponent},
-  {path: 'update', component: PageUpdateCustomerComponent},
 ];
 
 @NgModule({
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PageListUsersComponent} from "./pages/page-list-users/page-list-users.component";
-import {PageCreateUserComponent} from "./pages/page-create-user/page-create-user.component";
 import {PageReadUserComponent} from "./pages/page-read-user/page-read-user.component";
-import {PageUpdateUserComponent} from "./pages/page-update-user/page-update-user.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'list', pathMatch :'full'},
   {path: 'list', component: PageListUsersComponent},
-  {path: 'create', component: PageCreateUserComponent},
   {path: 'list/users/:id', component: PageReadUserComponent},
-  {path: 'update', component: PageUpdateUserComponent},
 
 ];
 
